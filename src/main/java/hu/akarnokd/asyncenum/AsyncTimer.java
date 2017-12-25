cfffc3ad789c20eb7a6b1e54037562d92992e6c0
akarnokd <akarnokd@gmail.com>
2017-12-25 18:08:56 +0100

    Add headers, testng, fix tests

--- src/main/java/hu/akarnokd/asyncenum/AsyncTimer.java
+++ src/main/java/hu/akarnokd/asyncenum/AsyncTimer.java
@@ -1,7 +1,22 @@
+/*
+ * Copyright 2017 David Karnok
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package hu.akarnokd.asyncenum;
 
 import java.util.concurrent.*;
-import java.util.function.LongPredicate;
 
 final class AsyncTimer implements AsyncEnumerable<Long> {
 
@@ -24,7 +39,7 @@ final class AsyncTimer implements AsyncEnumerable<Long> {
         return en;
     }
 
-    static final class TimerEnumerator implements AsyncEnumerator<Long>, Callable {
+    static final class TimerEnumerator implements AsyncEnumerator<Long>, Callable<Void> {
 
         final CompletableFuture<Boolean> single = new CompletableFuture<>();
 
@@ -48,7 +63,7 @@ final class AsyncTimer implements AsyncEnumerable<Long> {
         }
 
         @Override
-        public Object call() throws Exception {
+        public Void call() throws Exception {
             result = 0L;
             single.complete(true);
             return null;
--- src/test/java/hu/akarnokd/asyncenum/AsyncConcatArrayTest.java
+++ src/test/java/hu/akarnokd/asyncenum/AsyncConcatArrayTest.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2017 David Karnok
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package hu.akarnokd.asyncenum;
 
 import org.junit.Test;
@@ -24,6 +40,7 @@ public class AsyncConcatArrayTest {
     @Test
     public void simpleLong() throws Exception {
         List<Integer> list = new ArrayList<>();
+        @SuppressWarnings("unchecked")
         AsyncEnumerable<Integer>[] sources = new AsyncEnumerable[1_000_000];
         for (int i = 1; i < sources.length - 1; i++) {
             sources[i] = AsyncEnumerable.empty();
